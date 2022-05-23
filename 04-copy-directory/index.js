86ccab33754b11d16dbd521502cf214e70da36ab
PiachkoAliaksey <alek17y@mail.ru>
2022-05-24 02:00:52 +0300

    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs=require('fs');
+const path=require('path');
+
+
+/*fs.copyFile(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'), (err) => {
+    if (err) throw err;
+    console.log('Данные скопированы');
+})*/
+
+fs.mkdir(path.join(__dirname, 'files-copy'), () => {
+        
+});
+fs.readdir(path.join(__dirname, 'files'),(err)=>{
+    fs.copyFile(path.join(__dirname, 'files'), path.join(__dirname, 'files-copy'), (err) => {
+    if (err) throw err;
+    console.log('Данные скопированы');
+})
+
+    
+})
+
 

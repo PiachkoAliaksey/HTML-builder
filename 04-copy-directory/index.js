50683b1b460d1c812308ac23f1699ba503d4eee2
PiachkoAliaksey <alek17y@mail.ru>
2022-05-24 10:49:13 +0300

    refactor: fix task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,7 +8,7 @@ fs.mkdir(path.join(__dirname, 'files-copy'), () => {
 fs.readdir(path.join(__dirname, 'files'),(err, data)=>{
     for (let val of data){
         console.log(val)
-        fs.copyFile(path.join(__dirname,'files', val), path.join(__dirname, 'files-copy'), (err) => {
+        fs.copyFile(path.join(__dirname,'files', val), path.join(__dirname, 'files-copy',val), (err) => {
             if (err) throw err;
             console.log('Данные скопированы');
         })

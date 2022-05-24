4d3295f2c4ab22169ccd4f17a2d17a6bc38b020d
PiachkoAliaksey <alek17y@mail.ru>
2022-05-24 13:58:44 +0300

    fix: fix task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -7,17 +7,35 @@ fs.mkdir(path.join(__dirname, 'files-copy'), () => {
 });
 fs.readdir(path.join(__dirname, 'files'),(err, data)=>{
     for (let val of data){
-        console.log(val)
         fs.copyFile(path.join(__dirname,'files', val), path.join(__dirname, 'files-copy',val), (err) => {
             if (err) throw err;
             console.log('Данные скопированы');
         })
 
     }
+    fs.readdir(path.join(__dirname, 'files-copy'),(err, dataT)=>{
+       if(dataT.length !==data.length){
+        fs.rmdir(path.join(__dirname, 'files-copy'),{ recursive: true},(err)=>{
+            if (err) throw err;
+            fs.mkdir(path.join(__dirname, 'files-copy'), () => {
+        
+            });
+    
+            for (let val of data){
+                fs.copyFile(path.join(__dirname,'files', val), path.join(__dirname, 'files-copy',val), (err) => {
+                    if (err) throw err;
+                    console.log('Данные скопированы');
+                })
+        
+            }
+        })
+       
+        
+
+
+
+       }
+
     
 })
-
-    
-
-
-
+})
